6e3d6c09e38a3636fdd2fc022ca9f4ce45a9f114
Millzy <64277238+MillzyDev@users.noreply.github.com>
2023-01-20 22:44:43 +0000

    Update AssemblyInfo.cs

--- BSMLPlayground/Properties/AssemblyInfo.cs
+++ BSMLPlayground/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BSMLPlayground")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple Beat Saber mod used for speeding up UI dev.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BSMLPlayground")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
